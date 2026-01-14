9bb2bd9abc26a162d26d69791e0f21b1f3a61407
SpadeKim <vosgura2@gmail.com>
2026-01-14 20:44:14 +0900

    fix folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -633,7 +633,7 @@ export default function LostArkRefiningCalc() {
                     
                     <div className="space-y-2 pt-2 border-t mt-2">
                         {(() => {
-                            const baseProb = BASE_PROBABILITIES[simpleTargetLevel - 1] || 0;
+                            const baseProb = BASE_PROBABILITIES[simpleTargetLevel] || 0;
                             const stepValue = baseProb / 10; 
 
                             return (
